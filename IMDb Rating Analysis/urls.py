5365f73d24531829e04be71e82d16abe047558cf
Brightlat <227227Brightlat@users.noreply.github.com>
2023-09-22 00:32:03 +0500

    Removed Un-necessary Urls

--- IMDb Rating Analysis/urls.py
+++ IMDb Rating Analysis/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('txt/', include('txtFiles.urls')),
     path('csv/', include('csvFiles.urls'))
 ]
 
