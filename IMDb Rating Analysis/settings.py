d15ed1a2a50c36494ae2ef5ebc8622339d07ae8e
Demaurr <69788671+Demaurr@users.noreply.github.com.>
2025-01-14 16:27:51 +0500

    Updated settings with Debug off and Allowed Host *

--- IMDb Rating Analysis/settings.py
+++ IMDb Rating Analysis/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'csvFiles',
-    'pmdb_analysis',
 ]
 
 MIDDLEWARE = [
