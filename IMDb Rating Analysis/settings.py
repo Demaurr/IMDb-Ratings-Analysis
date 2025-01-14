3d9e8a8724023869501fd04d154b4bf1b4e678f1
Demaurr <69788671+Demaurr@users.noreply.github.com.>
2025-01-14 16:24:53 +0500

    Updated settings now using loadenv to access secret key

--- IMDb Rating Analysis/settings.py
+++ IMDb Rating Analysis/settings.py
@@ -1,9 +1,12 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open("secret_key.txt") as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
